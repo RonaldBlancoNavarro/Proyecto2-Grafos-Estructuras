1d69fb31000f3aac8a6697beabda5bb387979ae5
Roanld Blanco Navarro <ronald.blanco.navarro@est.una.ac.cr>
2021-11-08 19:57:12 -0600

    Agregar Pesos Aristas

--- ProjectoGrafos/Simulacion.cpp
+++ ProjectoGrafos/Simulacion.cpp
@@ -31,6 +31,11 @@ Simulacion::Simulacion() {
 	btnSalir = new Boton(centro - titulo.getGlobalBounds().width / 3.f, 500, 170, 80, &*font, "Salir", 25, Color::Red, Color::Green, Color::Black);
 	this->estadoSubmenu = MENU_PRINCIPAL;
 
+
+
+	permisoDigitar = false;
+	pesoArista = new Text();
+
 	//shape.setRadius(10);
 	//shape.setFillColor(Color::Red);
 }
@@ -67,6 +72,10 @@ void Simulacion::procesarEventos()
 	{
 		if (event.type == sf::Event::Closed) {
 			ventana->close();
+		}
+
+		if (event.type == sf::Event::TextEntered && permisoDigitar) {
+			modificarPesoArista(event);
 		}
 
 		// get mouse position
@@ -122,6 +131,16 @@ void Simulacion::graficarLineasAndPuntos() {
 			ventana->draw(v, 6, sf::Lines);
 		}
 	}
+	if (!pesosAristas.empty()) {
+		for (int i = 0;i < pesosAristas.size();i++) {
+			ventana->draw(pesosAristas[i]);
+		}
+	}
+
+	if (permisoDigitar) {
+		ventana->draw(*pesoArista);
+	}
+
 }
 
 void Simulacion::agregarVertice(Vector2f posMouse) {
@@ -135,6 +154,55 @@ void Simulacion::agregarVertice(Vector2f posMouse) {
 void Simulacion::agregarLinea() {
 	if (pos.size() == 2) {
 		lineas.push_back(pos);
+		//pos.clear();
+
+		permisoDigitar = true; // creacion de tecto de peso Arista
+		pesoArista = new Text();
+		pesoArista->setFont(*font);
+		pesoArista->setString("0");
+		pesoArista->setCharacterSize(20);
+		pesoArista->setFillColor(Color::Black);
+		pesoArista->setPosition( pos[0].x + ((pos[1].x - pos[0].x)/2), pos[0].y + ((pos[1].y - pos[0].y) / 2));
+
 		pos.clear();
+
 	}
+}
+
+
+void Simulacion::modificarPesoArista(Event event) {
+
+	//cout << event.text.unicode<<endl;
+//cout << (char)event.text.unicode<<endl;
+	//pesoArista->setString(pesoArista->getString() + (char)event.text.unicode);
+
+
+	if (event.text.unicode == 8) { // si va a borrar
+		if (pesoArista->getString().getSize() > 0) {
+			string palabra = pesoArista->getString();
+			size_t tam = palabra.length(); //obtenemos el tamaño del string
+			palabra.erase(tam - 1);
+
+			pesoArista->setString(palabra);
+		}
+	}
+
+	if (pesoArista->getString().getSize() <= 4) { // Si va a escribir .....limite de tamaño de peso de arista
+		if (event.text.unicode >= 48 && event.text.unicode <= 57) { // aceptacion de solo numeros
+			
+			if(event.text.unicode ) {
+				pesoArista->setString(pesoArista->getString() + (char)event.text.unicode);
+			}
+		}
+	}
+
+	if (event.text.unicode == 13) { // si ya acepto o termino la escritura con un enter
+		if (pesoArista->getString().getSize() == 0) { // si va vacia de peso la arista
+			pesoArista->setString("0");
+		}
+		permisoDigitar = false; // dejar de escribir
+		pesosAristas.push_back(*pesoArista); // agregar el peso de la arista
+		pesoArista = new Text();
+	}
+
 }
